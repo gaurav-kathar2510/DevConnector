7bfa01c76d66afdb61595568566fb7950b7d1bad
Gaurav Kathar <gkathar221@gmail.com>
2022-01-04 00:11:58 +0530

    Protected Route

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,17 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
-import { loadUser } from './actions/auth';
-import setAuthToken from './utils/setAuthToken';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
+
+
 
 //Redux
 
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 
@@ -33,6 +37,10 @@ const  App = () => {
             <Route path='/' element = {<Landing />}/>
             <Route path = "/register" element = {<Register />}/>
             <Route path = "/Login" element = {<Login />}/>
+            <Route
+            path="dashboard"
+            element={<PrivateRoute component={Dashboard} />}
+          />
 
           </Routes>
   </Router>
---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Dashboard = props => {
+    return (
+        <div>
+            Dashboard
+        </div>
+    )
+}
+
+Dashboard.propTypes = {
+    auth: PropTypes.object.isRequired
+}
+
+export default Dashboard
