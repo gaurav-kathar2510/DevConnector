b9990b4379ff617448341a38b1e7931a5b073126
Gaurav Kathar <gkathar221@gmail.com>
2022-01-03 23:34:05 +0530

    logout

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,60 +1,53 @@
 import {
     REGISTER_SUCCESS,
-    REGISTER_FAIL,
+    //REGISTER_FAIL,
     USER_LOADED,
     AUTH_ERROR,
     LOGIN_SUCCESS,
-    LOGIN_FAIL
-
-} from '../actions/types';
-
-const initialState = {
+    //LOGIN_FAIL,
+    LOGOUT,
+    ACCOUNT_DELETED
+  } from '../actions/types';
+  
+  const initialState = {
     token: localStorage.getItem('token'),
-    isAutheticated: null,
+    isAuthenticated: null,
     loading: true,
     user: null
-}
-
- function authtemp(state = initialState, action){
-
-    const{type, payload} = action;
-
-    switch(type){
-
-        case USER_LOADED:
-            return{
-                ...state,
-                isAutheticated: true,
-                loading: false,
-                user: payload
-
-            }
-        case REGISTER_SUCCESS:
-        case LOGIN_SUCCESS:
-            localStorage.setItem('token', payload.token);
-            return{
-                ...state,
-                ...payload,
-                isAutheticated: true,
-                loading: false
-            }
-
-        case REGISTER_FAIL:
-        case AUTH_ERROR:
-        case LOGIN_FAIL:
-            localStorage.removeItem('token');
-            return{
-                ...state,
-                token: null,
-                isAutheticated: false,
-                loading: false
-            }
-
-        default:
-            return state;
+  };
+  
+  function authReducer(state = initialState, action) {
+    const { type, payload } = action;
+  
+    switch (type) {
+      case USER_LOADED:
+        return {
+          ...state,
+          isAuthenticated: true,
+          loading: false,
+          user: payload
+        };
+      case REGISTER_SUCCESS:
+      case LOGIN_SUCCESS:
+        return {
+          ...state,
+          ...payload,
+          isAuthenticated: true,
+          loading: false
+        };
+      case ACCOUNT_DELETED:
+      case AUTH_ERROR:
+      case LOGOUT:
+        return {
+          ...state,
+          token: null,
+          isAuthenticated: false,
+          loading: false,
+          user: null
+        };
+      default:
+        return state;
     }
-
-
-}
-
-export default authtemp;
+  }
+  
+  export default authReducer;
